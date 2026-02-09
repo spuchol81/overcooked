10d18ae9698030b55af4920ef84c5855c210a949
spuchol81 <sylvain.puchol@gmail.com>
2026-02-09 19:28:56 +0100

    ml job

--- scripts/init.sh
+++ scripts/init.sh
@@ -128,4 +128,51 @@ curl -H "Authorization: ApiKey $ELASTICSEARCH_APIKEY" \
   }
 }'
 
+curl -H "Authorization: ApiKey $ELASTICSEARCH_APIKEY" \
+     -H "Content-Type: application/json" \
+     -X PUT "http://elasticsearch-es-http.default.svc:9200/_ml/data_frame/analytics/cooking_time_prediction" \
+     -d '
+{
+  "description": "Regression model to predict cooking duration",
+  "source": {
+    "index": [
+      "2025_cooking_stats"
+    ],
+    "query": {
+      "match_all": {}
+    }
+  },
+  "dest": {
+    "index": "cooking_time_prediction",
+    "results_field": "ml"
+  },
+  "analysis": {
+    "regression": {
+      "dependent_variable": "total_duration_minutes",
+      "prediction_field_name": "total_duration_minutes_prediction",
+      "training_percent": 80,
+      "randomize_seed": 3168482639878865400,
+      "loss_function": "mse",
+      "early_stopping_enabled": true,
+      "num_top_feature_importance_values": 0
+    }
+  },
+  "analyzed_fields": {
+    "includes": [
+      "ambient_temperature_c.avg",
+      "cook_id.keyword",
+      "meat_temperature_c.max",
+      "meat_temperature_c.min",
+      "recipe.keyword",
+      "total_duration_minutes"
+    ]
+  },
+  "model_memory_limit": "4mb",
+  "max_num_threads": 1,
+  "allow_lazy_start": false
+}'
+
+
+
+
 
