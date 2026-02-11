dafa21c567dfdb4986fd539bd5ef528560bdcbfb
spuchol81 <sylvain.puchol@gmail.com>
2026-02-11 16:00:04 +0100

    fix

--- scripts/init.sh
+++ scripts/init.sh
@@ -333,8 +333,8 @@ curl -H "Authorization: ApiKey $ELASTICSEARCH_APIKEY" \
   }
 }'
 
-cd ../dashboards
-curl -u "elastic:changeme" -H "Content-Type: application/json" -H "kbn-xsrf: true" -H "x-elastic-internal-origin: Kibana" -XPOST "http://kubernetes-vm:30001/api/saved_objects/_import?overwrite=true" -F "file=@dashboard.ndjson"
+
+curl -u "elastic:changeme" -H "kbn-xsrf: true" -H "x-elastic-internal-origin: Kibana" -XPOST "http://kubernetes-vm:30001/api/saved_objects/_import?overwrite=true" -F "file=@dashboards/bbq.ndjson"
 
 
 
